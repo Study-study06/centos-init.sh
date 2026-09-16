fcc84e99259654ea70d38ae0222e2d6605229b20
Renzo_cld <3623433788@qq.com>
2026-09-16 15:33:24 +0800

    Add files via upload
    之前脚本存在无法在同环境重复执行的弊端
    于是这次更新将之前无法重复执行的问题优化为可重复执行
    脚本依旧有些许弊端我将持续优化

---
+++ centos-init.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+  #----------------Centos自动化配置基础环境--------------------
+   # 脚本编写目的：解决上课、新建Linux测试环境时繁琐的基础环境配置过程
+   # 脚本测试的环境：New Centos7.9
+   # 编写作者：Renzo
+   #使用时请认真查看README.dm中的内容
+
+set -e
+
+HOST_NAME="Renzo"   #定义需要的用户名
+USER_IP="192.168.11.140"    #定义需要的主机IP地址
+IP_GW="192.168.11.2"      #定义需要的网关IP
+HOST_ENS="ens33"         #定义需要配置的网卡名
+HOST_YUM="https://mirrors.aliyun.com/repo/Centos-7.repo"   #定义需要的版本yum源
+          #https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo    centos8源
+HOSTS_IP="$USER_IP $HOST_NAME "   #配置主机映射调用的变量
+
+
+echo "[---------Start configuring the network environment----------]"
+hostnamectl set-hostname $HOST_NAME
+sed -i '/^IPADDR=.*/d;/^NETMASK=.*/d;/^GATEWAY=.*/d;/^DNS1=.*/d;/DNS2=.*/d' /etc/sysconfig/network-scripts/ifcfg-$HOST_ENS
+cat >> /etc/sysconfig/network-scripts/ifcfg-$HOST_ENS << EOF
+IPADDR=$USER_IP
+NETMASK=255.255.255.0
+GATEWAY=$IP_GW
+DNS1=8.8.8.8
+DNS2=114.114.114.114
+EOF
+sed -i 's/BOOTPROTO=.*/BOOTPROTO=static/g' /etc/sysconfig/network-scripts/ifcfg-$HOST_ENS
+systemctl restart network
+sleep 5
+sed -i '/^192/d' /etc/hosts
+cat >> /etc/hosts << EOF
+$HOSTS_IP
+EOF
+systemctl stop firewalld
+systemctl disable firewalld
+sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+ip a | grep $USER_IP
+ping -c 3 $HOST_NAME
+setenforce 0
+echo "[ The network environment has been set up! ]"
+read -p "Press Enter to continue..."
+
+
+echo "[---------Start configuring the yum repository----------]"
+mkdir -p /media/yum.repo.backup
+mv /etc/yum.repos.d/* /media/yum.repo.backup/
+curl -o /etc/yum.repos.d/Centos-Base.repo $HOST_YUM
+yum clean all && yum makecache
+sleep 5
+yum repolist
+echo "[ All yum repositories are backed up in the /media/ directory ]"
+echo "[ The yum repository has been configured! ]"
+echo "[ Basic environment configuration completed ]"
